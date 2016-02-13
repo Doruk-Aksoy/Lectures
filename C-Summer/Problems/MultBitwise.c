f96395fda3a3818bf4d3712dbfec7dd78d7bf9bb
Doruk Aksoy <Doruk-Aksoy@users.noreply.github.com>
2016-02-13 14:45:43 +0200

    Create MultBitwise.c

---
+++ C-Summer/Problems/MultBitwise.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+// Hardware logic (the last bit is sign bit, so carry will stop at sign bit and report it accordingly)
+int myAdd(unsigned int a, unsigned int b) {
+    unsigned int carry = a & b;
+    unsigned int result = a ^ b;
+    while(carry) {
+        unsigned int shiftedcarry = carry << 1;
+        carry = result & shiftedcarry;
+        result ^= shiftedcarry;
+    }
+    return result;
+}
+
+int myMult(int a, int b) {
+	int res = 0;
+	while(b) {
+		if (b & 1)
+			res = myAdd(res, a);
+		a <<= 1;
+		b >>= 1;
+	}
+}
+
+int main() {
+	int a, b;
+	printf("Enter two numbers (space seperated): ");
+	scanf("%d %d", &a, &b);
+	printf("Multiplication = %d", myMult(a, b));
+	return 0;
+}
